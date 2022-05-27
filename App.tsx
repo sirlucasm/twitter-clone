ab51afa7307194bfdf288ad8a870ab700a74f8fa
Lucas Matheus <lucasmatheus2021@gmail.com>
2022-05-26 21:16:10 -0300

    fix: added logbox ignore logs
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { AuthProvider } from './src/contexts/Auth';
 import { LogBox } from 'react-native';
 import Toast from 'react-native-toast-message';
 
-LogBox.ignoreLogs(['AsyncStorage']);
+LogBox.ignoreLogs(['AsyncStorage', 'Setting a timer', 'Found screens']);
 
 export default function App() {
   return (
