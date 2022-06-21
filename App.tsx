610b52a8de0a2dc87ce52cb63edd43848653eb40
Lucas Matheus <lucasmatheus2021@gmail.com>
2022-06-21 12:32:07 -0300

    fix: tweet post bug solved
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import { AuthProvider } from './src/contexts/Auth';
 import { LogBox } from 'react-native';
 import Toast from 'react-native-toast-message';
 
-LogBox.ignoreLogs(['AsyncStorage', 'Setting a timer', 'Found screens']);
+LogBox.ignoreLogs([
+  'AsyncStorage',
+  'Setting a timer',
+  'Found screens',
+  'Non-serializable values were found in the navigation state',
+]);
 
 export default function App() {
   return (
